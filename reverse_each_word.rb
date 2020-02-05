e83d7c6a68c58aca9f3787a9db39d8fcdfa9a10a
JT Lu <alohajt94@gmail.com>
2020-02-05 22:17:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,17 @@
-# def reverse_each_word(str)
-#   arr = str.split(" ")
-#   reverseArr = []
-#   arr.each {|arr| reverseArr << str.reverse}
-#   reverseArr.join(" ")
+def reverse_each_word(str)
+  arr = str.split(" ")
+  reverseArr = []
+  arr.each {|str| reverseArr << str.reverse}
+  reverseArr.join(" ")
   
-# end
-
-
-def reverse_each_word(string)
-  original_array = string.split(" ")
-  return_array = []
-  original_array.each do|string|
-    return_array << string.reverse
-  end
-  return_array.join(" ")
 end
+
+
+# def reverse_each_word(string)
+#   original_array = string.split(" ")
+#   return_array = []
+#   original_array.each do|string|
+#     return_array << string.reverse
+#   end
+#   return_array.join(" ")
+# end
